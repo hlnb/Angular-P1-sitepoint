c06abca0b7410295e773961d464a95d9e9729efd
Elizabeth H Burgess <heb264@gmail.com>
2017-12-05 14:38:58 +0800

    debugging after finishing tutorial

---
+++ p1-skeleton/app/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//node imports
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+//module
+var app_module_1 = require("./app.module");
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
