0398323de3207947c02329e66888c4774052333b
Elizabeth H Burgess <heb264@gmail.com>
2017-12-05 14:41:38 +0800

    finished debugging and project works

--- p1-skeleton/systemjs.config.js
+++ p1-skeleton/systemjs.config.js
@@ -10,12 +10,14 @@
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api'
         },
+
         packages: {
             app: {
                 main: './main.js',
@@ -29,6 +31,5 @@
                 defaultExtension: 'js'
             }
         }
-
     });
 })(this);
